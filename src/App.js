2cfa9c6ad9584ab6c1b65c6eb22c6bf97bf68186
MateusFattori <83764798+MateusFattori@users.noreply.github.com>
2022-03-16 22:03:17 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import axios from "axios"
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemeonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState("")
+
+  const getPokemon = async () => {
+    const toArray = []
+    try{
+
+    } catch (e) {
+
+    }
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
-  );
+  )
 }
 
 export default App;
