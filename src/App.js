6f6032b625b4ba09f084db1be7cedeccb347cbb4
MateusFattori <83764798+MateusFattori@users.noreply.github.com>
2022-03-17 21:52:54 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios"
 import './App.css';
 
@@ -10,15 +10,63 @@ const App = () => {
   const getPokemon = async () => {
     const toArray = []
     try{
-
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+      const res = await axios.grt(url);
+      toArray.push(res.date)
+      setPokemonType(res.date.type[0].type.name)
+      setPokemonData(toArray)
+      console.log(res);
     } catch (e) {
-
+      console.log(e);
     }
   }
 
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase())
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    getPokemon()
+  }
+
+  useEffect(() => {
+    getPokemon();
+  }, [])
+
   return(
     <div className="App">
-      <h1>Hello</h1>
+      <form onSubmit={handleSubmit}> 
+        <label>
+          <input type="text" onChange={handleChange} placehoder="Enter Pokemon Name"/>
+        </label>
+      </form>
+      {pokemeonData.map((data) => {
+        return(
+          <div className="container">
+            <img />
+            <div className="divTable">
+              <div className="divTableBody"></div>
+              <div className="divTableRow">
+                <div className="divTableCell">Type</div>
+                <div className="divTableCell">{pokemonType}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Height</div>
+                <div className="divTableCell">{pokemonType}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Type</div>
+                <div className="divTableCell">{pokemonType}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Type</div>
+                <div className="divTableCell">{pokemonType}</div>
+              </div>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
